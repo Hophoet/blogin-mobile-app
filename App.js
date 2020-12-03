449023522ea861f0009e6c836bec64c73ec8ade1
hophoet <hophoet@gmail.com>
2020-12-03 20:44:24 +0000

    (state) - intial commit with usefull installed package for navigations, local storage, icons

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Home from './screens/Home'
+
+
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <Home/>
+    );
+  }
+}
+
+
