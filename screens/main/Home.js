22c07a9e9b07e6e2862e9c2bf702c7cd317973db
hophoet <hophoet@gmail.com>
2020-12-13 00:05:55 +0000

    (methods) create navigation method in the home screen, pass the created navigation method to HomeScreenBody components props

--- screens/main/Home.js
+++ screens/main/Home.js
@@ -7,13 +7,18 @@ import HomeScreenBody from '../../components/HomeScreenBody'
 export default class Home extends React.Component {
     constructor(props){
         super(props)
+        this.posts = require('../../data/posts.json')
+    }
+
+    navigate = (screenName, data) => {
+        this.props.navigation.navigate(screenName, data)
     }
 
     render(){
         return (
             <View style={styles.container}>
                 <HomeScreenHeader/>
-                <HomeScreenBody/>
+                <HomeScreenBody navigate={this.navigate} posts={this.posts} />
                 <Text>home screen</Text>
             </View>
         )
