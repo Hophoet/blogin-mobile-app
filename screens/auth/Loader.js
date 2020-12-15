90c407abf29dbb92557e6c13dda90969b7e3b918
hophoet <hophoet@gmail.com>
2020-12-15 15:51:52 +0000

    (methods) create methods for authentification token managment

--- screens/auth/Loader.js
+++ screens/auth/Loader.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Text, StyleSheet, View, ActivityIndicator} from 'react-native'
+import AsyncStorage from '@react-native-community/async-storage';import Icon from "react-native-vector-icons/Ionicons";
 //colors
 import {colors} from '../../assets/colors/colors'
 
@@ -8,6 +9,76 @@ export default class Loader extends React.Component{
     constructor(props){
         super(props)
     }
+
+    
+            
+        
+     
+
+        _tokenIsSends = () => {
+            let tokenIsSend = false
+            let params = this.props.navigation.state.params
+            if(params){
+                if(params.token){
+                    tokenIsSend = true
+                }
+            }
+            return tokenIsSend
+        }
+
+        _tokenExists = () => {
+            const keys = await AsyncStorage.getAllKeys()
+            const values = await AsyncStorage.multiGet(keys)
+            let tokenExists = false 
+            values.forEach( item => {
+                if(item){
+                    if(item[0] === 'token'){
+                        tokenExists = true
+                        
+                    }
+                }
+            })
+            return tokenExists
+        }
+
+        _getSendTokenFromAuth = () =>{
+            let params = this.props.navigation.state.params
+            token = false
+            if(params){
+                if(params.token){
+                    token = params.token
+                }
+            }
+            return token
+        }
+
+        _getSaveToken = () => {
+            const keys = await AsyncStorage.getAllKeys()
+            const values = await AsyncStorage.multiGet(keys)
+            let tokenExists = false 
+            values.forEach( item => {
+                if(item){
+                    if(item[0] === 'token'){
+                        tokenExists = item[1]
+                    }
+                }
+            })
+            return tokenExists
+        }
+
+
+        //save token
+        _saveToken = (token) => {
+            AsyncStorage.setItem('token', token)
+            .then(()=>{
+                //save case
+            })
+        }
+
+        componentDidMount(){
+           
+        }
+
 
     render(){
         return (
