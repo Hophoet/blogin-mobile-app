b635ea7e663f52a8a9a88e5af729ef9724111643
hophoet <hophoet@gmail.com>
2020-12-19 11:40:41 +0000

    (assets) - add app core sizes package

---
+++ assets/sizes/sizes.js
@@ -0,0 +1,11 @@
+import  {Dimensions} from 'react-native'
+
+
+const window = {
+    "height":Dimensions.get('window').height,
+    "width":Dimensions.get("window").width,
+}
+
+export{
+    window
+}
