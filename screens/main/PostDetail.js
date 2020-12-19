3b16b069ddf0674b260d923e685af13dbc4f8645
hophoet <hophoet@gmail.com>
2020-12-19 13:57:47 +0000

    (screens) - update post detail screen with post like component, fload button

--- screens/main/PostDetail.js
+++ screens/main/PostDetail.js
@@ -1,35 +1,130 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Image, ScrollView, Dimensions, FlatList, TouchableOpacity, TouchableWithoutFeedback} from 'react-native'
 //color
 import {colors} from '../../assets/colors/colors'
+//sizes
+import  {window} from '../../assets/sizes/sizes'
+//
+import Icon from 'react-native-vector-icons/Ionicons'
 //components
 import PostDetailScreenHeader from '../../components/PostDetailScreenHeader'
-
+import PostCommentItem from '../../components/PostCommentItem'
+import FloatButton from '../../components/buttons/FloatButton'
 export default class PostDetail extends React.Component{
     constructor(props){
         super(props)
         this.post = this.props.navigation.state.params.post
+        this.state = {
+            postIsLiked:false
+        }
+    }
+
+    getPostLikeIconLabel = () => {
+        if(this.state.postIsLiked){
+            return 'heart'
+        }
+        return 'heart-outline'
+    }
+
+    togglePostLike = () => {
+        this.setState({postIsLiked:!this.state.postIsLiked});
     }
 
     navigate = (screenName, data) => {
         this.props.navigation.navigate(screenName, data)
     }
 
+
     render(){
        const headerData = { 'postTitle':this.post.title}
         return (
             <View style={styles.container}>
-                <PostDetailScreenHeader headerData={headerData}  />
-                <Text>{this.post.title}</Text>
-                <Text style={styles.postContent}>{this.post.content}</Text>
-                <Text onPress={() => this.navigate('Home') }>GOBACK</Text>
+                <PostDetailScreenHeader navigate={this.navigate} headerData={headerData}  />
+           
+                
+                <ScrollView 
+                    style={styles.scrollView}
+                    showsVerticalScrollIndicator={false}
+                >
+                    <View style={styles.imageContainer}>
+                        <Image resizeMode='contain' style={styles.image} source={require('../../assets/images/pis.jpg')}/>
+                    </View>
+              
+                    <View style={styles.bodyContainer}>
+                    <TouchableOpacity style={styles.postLikeIconContainer}  onPress={() => {this.togglePostLike()}} >
+                        <Icon name={this.getPostLikeIconLabel()} size={window.width/7} />
+                    </TouchableOpacity>
+            
+                        <Text style={styles.title}>{this.post.title}</Text>
+                        <Text style={styles.content}>{this.post.content}</Text>
+                        <Text style={styles.content}>{this.post.content}</Text>
+                        <View style={styles.commentsContainer}>
+                            {/* <Text style={styles.commentsPartTitle}>comments</Text>
+                            <FlatList
+                                style={styles.commentsFlatList}
+                                data={this.post.comments}
+                                keyExtractor={(item) => item.user.toString()}
+                                renderItem={({index, item}) => <PostCommentItem comment={item}/> }
+
+
+                            /> */}
+                        </View>
+                    </View>
+                </ScrollView>
+                <FloatButton/>
             </View>
         )
     }
 }
 
+const {height, width} = Dimensions.get('window')
 const styles = StyleSheet.create({
     container:{
+        flex:1
+    },
+    image:{
+        height:width
+    },
+    imageContainer:{
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    bodyContainer:{
+        marginHorizontal:7,
+        backgroundColor:'white',
+    },
+    title:{
+        alignSelf:'center',
+        textAlign:'center',
+        margin:10,
+        fontSize:20,
+        fontWeight:'bold'
+    },
+    content:{
+        alignSelf:'center',
+        // textAlign:'center',
+        margin:5
+    },
+    commentsContainer:{
+        // backgroundColor:'gray',
 
+    },
+    commentsPartTitle:{
+        alignSelf:'center',
+        textAlign:'center',
+    },
+    scrollView:{
+        flex:1
+    },
+    postLikeIconContainer:{
+        // backgroundColor:'red',
+        // position:'absolute',
+        elevation:5,
+        justifyContent:'center',
+        alignItems:'center',
+        // right:window.width/10,
+        // bottom:window.width/10,
+        
+        
     }
 })
