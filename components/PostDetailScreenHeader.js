ac5a71e3ba61c39e14d55b977bbfbdcfdb95dde0
hophoet <hophoet@gmail.com>
2020-12-19 12:37:34 +0000

    (components) - update post detail screen header component (back arrow and post title as )

--- components/PostDetailScreenHeader.js
+++ components/PostDetailScreenHeader.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import {StyleSheet, View , Text, Dimensions} from 'react-native'
+import {StyleSheet, View , Text, Dimensions, TouchableOpacity} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+//sizes
+import {window} from '../assets/sizes/sizes'
 
 export default (props) => {
     const headerData = props.headerData
     return (
         <View style={styles.container}>
-            <View style={[styles.column]}>
-                <Text numberOfLines={1}>{'<'}  {headerData.postTitle}</Text>
+            <View style={[styles.header]}>
+                <TouchableOpacity 
+                    onPress={() => {props.navigate("Home",{})}}
+                    style={styles.backButtonContainer}>
+                    <Icon name='arrow-back-outline' size={window.width/15} />
+                </TouchableOpacity>
+                <View style={styles.postHeaderTitleContainer}>
+                    <Text numberOfLines={1} style={styles.postHeaderTitle}> {headerData.postTitle}</Text>
+                </View>
             </View>
             <View style={[styles.column]}>
                 <Text></Text>
@@ -19,14 +29,30 @@ const {height, width} = Dimensions.get('window')
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor:'gray',
         justifyContent:'space-between',
-        padding:height/40,
         flexDirection:'row',
+        padding:window.width/20,
         
+    },
+    header:{
+        flexDirection:'row',
+        flex:1,
     },
     column:{
         // backgroundColor:'#584739',
         margin:2
-    }
+    },
+    backButtonContainer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    postHeaderTitle:{
+       
+    },
+    postHeaderTitleContainer:{
+        flex:5,
+        justifyContent:'center'
+    },
+
 })
