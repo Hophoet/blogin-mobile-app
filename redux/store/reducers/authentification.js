3a499c6650e42ed16848fb719689b7efbe96ce0c
hophoet <hophoet@gmail.com>
2020-12-22 18:12:21 +0000

    (redux) - add authentification reducer

---
+++ redux/store/reducers/authentification.js
@@ -0,0 +1,18 @@
+import {SET_AUTHENTIFICATION_TOKEN} from './actions'
+
+//set initial state
+const initialState = {
+}
+
+// This is a reducer which listens to actions and modifies the state
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_AUTHENTIFICATION_TOKEN:
+        const authentificationToken = action.value
+      return {
+        ...state,
+        authentificationToken: authentificationToken
+      }
+    default:
+      return state
+    }
