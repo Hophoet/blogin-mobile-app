ffd63b3def68b053b2082682eaa5420d3f5da74e
hophoet <hophoet@gmail.com>
2020-12-22 18:33:00 +0000

    (redux) - update main component with redux and redux-persist configurations

--- App.js
+++ App.js
@@ -7,14 +7,27 @@
  */
 
 import React from 'react';
+
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import Store from './redux/store/storeConfigurations'
+import { persistStore } from 'redux-persist'
+
 import Main from './navigations/Navigators'
 
 
 
 export default class App extends React.Component{
   render(){
+    let persistor = persistStore(Store)
+
     return (
-      <Main/>
+      <Provider store={Store}>
+        <PersistGate persistor={persistor}>
+          <Main/>
+        </PersistGate>
+      </Provider>
     );
   }
 }
