5fb32c02304a4c1e3416db6d775a3c348f2817c8
hophoet <hophoet@gmail.com>
2020-12-22 18:02:39 +0000

    (redux) - add authentification auth setting action

---
+++ redux/store/actions.js
@@ -0,0 +1,11 @@
+//Authentification
+const SET_AUTHENTIFICATION_TOKEN = 'SET_AUTHENTIFICATION_TOKEN'
+const setAuthentificationTokenAction = {
+    type: setAuthentificationTokenAction
+}
+
+
+
+export default {
+    setAuthentificationTokenAction
+}
