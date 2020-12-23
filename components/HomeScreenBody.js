2f572263b863a655d2158943a692b450946c2c48
hophoet <hophoet@gmail.com>
2020-12-23 16:05:08 +0000

    (components) - remove unused props

--- components/HomeScreenBody.js
+++ components/HomeScreenBody.js
@@ -8,14 +8,13 @@ import PostItem from '../components/PostItem'
 export default class HomeScreenBody extends React.Component{
     constructor(props){
         super(props);
-        this.authToken = this.props.authToken
+        
     }
     componentDidMount(){
     }
     render(){
     //get posts list from props
     const posts = this.props.posts
-    // console.log('home screen '+this.authToken)
 
     // console.log('Home screen body posts props')
     // console.log(typeof posts)
@@ -24,10 +23,10 @@ export default class HomeScreenBody extends React.Component{
            <View style={styles.postsContainer}>
                <Text>Posts</Text>
                <FlatList
-                
+                    
                     data={posts}
                     keyExtractor={({index, item}) => index}
-                    renderItem={({index, item}) => <PostItem authToken={this.authToken} navigate={this.props.navigate} post={item}/>  }
+                    renderItem={({index, item}) => <PostItem navigate={this.props.navigate} post={item}/>  }
 
                  />
            </View>
