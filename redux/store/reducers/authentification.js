a7e0975c4b1a93ef61a0322b5be6813f16804939
hophoet <hophoet@gmail.com>
2020-12-26 18:49:01 +0000

    (redux) - add remove user authentification token case to the reducer

--- redux/store/reducers/authentification.js
+++ redux/store/reducers/authentification.js
@@ -1,4 +1,4 @@
-import {SET_AUTHENTIFICATION_TOKEN, GET_USER_PROFILE} from '../actions'
+import {SET_AUTHENTIFICATION_TOKEN, GET_USER_PROFILE, REMOVE_AUTHENFICATION_TOKEN} from '../actions'
 
 //set initial state
 const initialState = {
@@ -7,7 +7,7 @@ const initialState = {
 // This is a reducer which listens to actions and modifies the state
 const reducer = (state = initialState, action) => {
   let newState
-  console.log('call auth reducer')
+  // console.log('call auth reducer')
   switch (action.type) {
     case SET_AUTHENTIFICATION_TOKEN:
         const authentificationToken = action.value
@@ -19,10 +19,22 @@ const reducer = (state = initialState, action) => {
         return newState
     case GET_USER_PROFILE:
         const userProfileObject = action.value
+        // console.log('call user profile reducer , value:')
+        // console.log(userProfileObject)
         newState = {
             ...state,
             userProfile:userProfileObject
         }
+        console.log('new state from redux')
+        console.log(newState)
+        return newState
+    case REMOVE_AUTHENFICATION_TOKEN:
+        newState = { ...state }
+        if(newState.hasOwnProperty('authentificationToken')){
+            delete newState.authentificationToken
+        }
+        // console.log('REMOVE AUTH TOKEN REDUCER')
+        // console.log(newState)
         return newState
     default:
       return state
